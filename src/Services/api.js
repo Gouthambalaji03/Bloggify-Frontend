6a793d525d4c4e11513c1951606919b4f706b794
Goutham Balaji P S <26gouthamba@gmail.com>
2025-12-17 04:16:06 +0530

    url updated

--- src/Services/api.js
+++ src/Services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://bloggify-backend-oynh.onrender.com",
+  baseURL: "https://bloggify-backend-oynh.onrender.com/api",
 });
 
 api.interceptors.request.use((config) => {
